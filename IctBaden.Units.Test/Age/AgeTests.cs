8ceeb78ddc906bcd3c0a6b5b44c2c6dbcf4215fa
Frank Pfattheicher <fpf@ict-baden.de>
2023-03-07 11:49:30 +0100

    AgeTests w/culture

--- IctBaden.Units.Test/Age/AgeTests.cs
+++ IctBaden.Units.Test/Age/AgeTests.cs
@@ -1,39 +1,46 @@
 using System;
+using System.Globalization;
 using Xunit;
 
-namespace IctBaden.Units.Test.Age
+namespace IctBaden.Units.Test.Age;
+
+public class AgeTests
 {
-    public class AgeTests
+    private readonly CultureInfo _culture;
+
+    public AgeTests()
     {
-        [Fact]
-        public void AgeShouldBeZeroAtBirthdate()
-        {
-            var age = new Units.Age(DateTime.Parse("2.10.1959"), DateTime.Parse("2.10.1959"));
-            
-            Assert.Equal(0, age.Years);
-            Assert.Equal(0, age.Months);
-            Assert.Equal(0, age.Days);
-        }
-        
-        [Fact]
-        public void AgeDaysAndMonthsShouldBeZeroAtBirthday()
-        {
-            var age = new Units.Age(DateTime.Parse("2.10.1959"), DateTime.Parse("2.10.2059"));
-            
-            Assert.Equal(100, age.Years);
-            Assert.Equal(0, age.Months);
-            Assert.Equal(0, age.Days);
-        }
-        
-        [Fact]
-        public void AgeShouldBeCorrect()
-        {
-            var age = new Units.Age(DateTime.Parse("2.10.1959"), DateTime.Parse("6.12.2021"));
-            
-            Assert.Equal(62, age.Years);
-            Assert.Equal(2, age.Months);
-            Assert.Equal(4, age.Days);
-        }
-        
+        _culture = new CultureInfo("de-DE");
     }
+        
+    [Fact]
+    public void AgeShouldBeZeroAtBirthdate()
+    {
+        var age = new Units.Age(DateTime.Parse("2.10.1959", _culture), DateTime.Parse("2.10.1959", _culture));
+            
+        Assert.Equal(0, age.Years);
+        Assert.Equal(0, age.Months);
+        Assert.Equal(0, age.Days);
+    }
+        
+    [Fact]
+    public void AgeDaysAndMonthsShouldBeZeroAtBirthday()
+    {
+        var age = new Units.Age(DateTime.Parse("2.10.1959", _culture), DateTime.Parse("2.10.2059", _culture));
+            
+        Assert.Equal(100, age.Years);
+        Assert.Equal(0, age.Months);
+        Assert.Equal(0, age.Days);
+    }
+        
+    [Fact]
+    public void AgeShouldBeCorrect()
+    {
+        var age = new Units.Age(DateTime.Parse("2.10.1959", _culture), DateTime.Parse("6.12.2021", _culture));
+            
+        Assert.Equal(62, age.Years);
+        Assert.Equal(2, age.Months);
+        Assert.Equal(4, age.Days);
+    }
+        
 }
